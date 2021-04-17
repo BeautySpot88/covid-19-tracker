3a9126201d42944161c7162fabacc2411b7ee987
Beauty Spot <75646530+BeautySpot88@users.noreply.github.com>
2021-04-18 00:29:51 +0100

    created dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { MenuItem, Select, FormControl } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  //
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Option 2</MenuItem>
+            <MenuItem value="worldwide">Clearly</MenuItem>
+  <MenuItem value="worldwide">Yoo</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
+
+      {/*Header*/}
+      {/*Title + select input dropdown field*/}
+      {/*InfoBoxs*/}
+      {/*InfoBoxs*/}
+      {/*InfoBoxs*/}
+      {/*Table*/}
+      {/*Graph*/}
+      {/*Map*/}
     </div>
   );
 }
